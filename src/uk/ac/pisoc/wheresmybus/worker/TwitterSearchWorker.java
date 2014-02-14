d06390fc399dc9a66a2de0cfb709457c102851b8
Ben-Farnfield <ben.farnfield@gmail.com>
2014-02-14 23:25:00 +0000

    implemented new cache StatusIdCache within TwitterSearchWorker

--- src/uk/ac/pisoc/wheresmybus/worker/TwitterSearchWorker.java
+++ src/uk/ac/pisoc/wheresmybus/worker/TwitterSearchWorker.java
@@ -1,6 +1,5 @@
 package uk.ac.pisoc.wheresmybus.worker;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
 
@@ -9,73 +8,75 @@ import twitter4j.QueryResult;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
+import uk.ac.pisoc.wheresmybus.cache.StatusIdCache;
 import uk.ac.pisoc.wheresmybus.logger.Logger;
 import uk.ac.pisoc.wheresmybus.model.HashtagTweet;
 
 public class TwitterSearchWorker extends Worker {
 	
 	private static final String TAG = "TwitterSearchWorker";
+
+	private final int CACHE_SIZE = 300;
+	private StatusIdCache statusIdCache = new StatusIdCache( CACHE_SIZE );
 	
-	private List<Status> tweetCache;
-	
-	private Query        twitterQuery;
-	private QueryResult  queryResult;
+	private Query twitterQuery;
+	private QueryResult queryResult;
 		
-	public TwitterSearchWorker(BlockingQueue<HashtagTweet> bq, Twitter twitter,
-			String threadName, String query) {
-		
-		super(bq, twitter, threadName);
-		
-		tweetCache = new ArrayList<>(); //TODO purge old tweets
-		twitterQuery = new Query(query);
+	public TwitterSearchWorker( BlockingQueue<HashtagTweet> bq,
+			                    Twitter twitter,
+			                    String threadName, 
+			                    String query )
+	{	
+		super( bq, twitter, threadName );
+		twitterQuery = new Query( query );
+		twitterQuery.setCount( 100 ); // max number of results
 	}
 	
 	@Override
 	public void run() {
 		super.run();
-		
-		// Load the tweetCache with old tweets
-		Logger.log(TAG, "searching for old tweets ...");
-		List<Status> tweets = search();
-		for (Status tweet : tweets) {
-			if (tweet.getGeoLocation() != null) {
-				tweetCache.add(tweet);
-			}
-		}
-		Logger.log(TAG, "cache loaded with old tweets.");
 
-		// Search for new tweets
+		primeCache();
+
 		for (;;) {
-			Logger.log(TAG, "searching twitter ...");
+			Logger.log( TAG, "searching twitter ..." );
 			
 			try {
-				tweets = search();
+				List<Status> tweets = search();
 				
-				for (Status tweet : tweets) {
-					// Check geolocation data has been included.
-					if (tweet.getGeoLocation() != null) {
-						// Check we've not seen this tweet before.
-						if (! tweetCache.contains(tweet)) {
-							Logger.log(TAG, "found tweet from: @" 
-					                + tweet.getUser().getScreenName());
-							tweetCache.add(tweet);
-							bq.put(new HashtagTweet(tweet));
-						}
+				for ( Status tweet : tweets ) {
+					if ( tweet.getGeoLocation() != null &&
+						 !statusIdCache.contains( tweet.getId() )) {
+						
+						Logger.log( TAG, "found tweet from: @" 
+				                + tweet.getUser().getScreenName() );
+						
+						statusIdCache.add( tweet.getId() );
+						bq.put( new HashtagTweet(tweet) );
 					}
 				}
-				
-				sleep(8000l); // Wait 8 seconds (5 is the min we could set).
-
-			} catch (InterruptedException e) {}
+				sleep(8000l); // Wait 8 seconds
+			} 
+			catch ( InterruptedException e ) {}
 		}
+	}
+
+	private void primeCache() {
+		List<Status> tweets = search();
+		for ( Status tweet : tweets ) {
+			if (tweet.getGeoLocation() != null) {
+				statusIdCache.add( tweet.getId() );
+			}
+		}
+		Logger.log( TAG, "cache primed with old tweets." );
 	}
 	
 	private List<Status> search() {
 		try {
-			queryResult = twitter.search(twitterQuery);
-		} catch (TwitterException e) {
+			queryResult = twitter.search( twitterQuery );
+		} catch ( TwitterException e ) {
 			e.printStackTrace();
-			Logger.log(TAG, "search failed : " + e.getMessage());
+			Logger.log( TAG, "search failed : " + e.getMessage() );
 		}
 		return queryResult.getTweets();
 	}
